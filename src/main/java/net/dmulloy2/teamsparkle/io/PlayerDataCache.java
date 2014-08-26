4765f32863283cdda081fd26f0ceb15a22a97b0d
Dan Mulloy <dmulloy2@live.com>
2014-08-25 20:24:51 -0400

    Update due to changes in SwornAPI

--- src/main/java/net/dmulloy2/teamsparkle/io/PlayerDataCache.java
+++ src/main/java/net/dmulloy2/teamsparkle/io/PlayerDataCache.java
@@ -17,12 +17,12 @@ import java.util.concurrent.Future;
 import java.util.logging.Level;
 
 import net.dmulloy2.io.FileSerialization;
+import net.dmulloy2.io.IOUtil;
 import net.dmulloy2.io.UUIDFetcher;
 import net.dmulloy2.teamsparkle.TeamSparkle;
 import net.dmulloy2.teamsparkle.types.PlayerData;
 import net.dmulloy2.types.Versioning;
 import net.dmulloy2.types.Versioning.Version;
-import net.dmulloy2.util.FormatUtil;
 import net.dmulloy2.util.Util;
 
 import org.bukkit.OfflinePlayer;
@@ -202,7 +202,7 @@ public class PlayerDataCache
 
 		for (File file : files)
 		{
-			String fileName = FormatUtil.trimFileExtension(file, extension);
+			String fileName = IOUtil.trimFileExtension(file, extension);
 			if (! isFileLoaded(fileName))
 				data.put(fileName, loadData(fileName));
 		}
@@ -316,7 +316,7 @@ public class PlayerDataCache
 
 		for (File file : files)
 		{
-			String fileName = FormatUtil.trimFileExtension(file, extension);
+			String fileName = IOUtil.trimFileExtension(file, extension);
 			PlayerData loaded = loadData(fileName);
 			loaded.setLastKnownBy(fileName);
 			data.put(fileName, loaded);
